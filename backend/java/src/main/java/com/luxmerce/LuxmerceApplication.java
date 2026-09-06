8ddb02b477a64fb280f8c813a75c3c339d1f8d57
Jordan Leturgez <jordanleturgez@gmail.com>
2026-09-06 18:21:01 -0400

    Initialize LuxmerceApplication with Spring Boot

---
+++ backend/java/src/main/java/com/luxmerce/LuxmerceApplication.java
@@ -0,0 +1,11 @@
+package com.luxmerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LuxmerceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(LuxmerceApplication.class, args);
+    }
+}
